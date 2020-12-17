78d0fb39658f41263f91cf755e0afad65c412bc2
정종호 <jujung@kimzipsa.co.kr>
2020-12-18 04:08:08 +0900

    수정

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :pty, true
 append :linked_files, "config/database.yml",".env"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/assets", "public/uploads"}
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/assets", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
