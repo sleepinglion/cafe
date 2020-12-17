619be36deb4f9d5fdb59fb8ced658b98ee0d1774
정종호 <jujung@kimzipsa.co.kr>
2020-12-18 03:29:42 +0900

    수정

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class PointsController < ApplicationController
   load_and_authorize_resource
   before_action :set_User, only: [:show, :edit, :update, :destroy]
 
